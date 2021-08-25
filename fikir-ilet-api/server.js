40c411988b98a138c800d7e2f4486dbfc7ca38a7
MertbabaOkulmus <mertbaba.okulmus@hotmail.com>
2021-08-25 13:41:00 +0300

    admin sheması oluşturuldu ve fikirler in gösterilmesi için gerekli get/post kanalları açıldı

--- fikir-ilet-api/server.js
+++ fikir-ilet-api/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Article = require('./models/Article')
+const Fikir = require('./models/Fikir')
+const Admin =require('./models/Admin');
 
 const app = express();
 app.use(cors());
@@ -28,25 +29,41 @@ app.get('/kullanici', (req, res) => {
 
 app.post('/fikirkaydet', (req, res) => {
 
-  /* const {fullname,email,typeofidea,idea} = req.body;
-   Article.create({
+   const {fullname,email,typeofidea,idea} = req.body;
+   Fikir.create({
         fullname,
         email,
         typeofidea,
         idea
    },err=>{
-       if(err) throw err;
-       console.log("Kayıt başarılı");
-   })*/
+       if(err) res.sendStatus(400);
+       res.sendStatus(200);
+       
+   })
 
-   // console.log(req.body);
+  /* console.log(req.body);
     var new_save = new Article(req.body)
      console.log(new_save);
      new_save.save((err) => {
         if (err) throw err
         consol.log("Veri başarı ile kaydedildi!");
-    })
+    })*/
+})
 
+app.post('/admin',(req,res)=>{
+    const {fullName,password}=req.body;   
+    Admin.find({fullname:fullName})
+    .then(doc=> {
+        if(doc[0].password  != password ) {}
+        else res.sendStatus(200)
+    })
+})
+
+app.get('/fikirler',(req,res)=>{
+    Fikir.find()
+    .then(docs=>{
+        res.send(docs)
+    })
 })
 
 app.listen(5555)
